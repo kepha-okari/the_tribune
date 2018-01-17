97a9e65e708f2653e324b9e12e3f92da62bb5dec
rkepha <kephaokari@gmail.com>
2018-01-17 18:02:56 +0300

    aded route and template to display single article

--- tribune/news/models.py
+++ tribune/news/models.py
@@ -31,6 +31,8 @@ class Article(models.Model):
     editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
+    article_image = models.ImageField(upload_to = 'articles/',blank =True)
+
 
     @classmethod
     def todays_news(cls):
@@ -41,4 +43,9 @@ class Article(models.Model):
     @classmethod
     def days_news(cls,date):
         news = cls.objects.filter(pub_date__date = date)
+        return news
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        news = cls.objects.filter(title__icontains=search_term)
         return news
--- tribune/news/views.py
+++ tribune/news/views.py
@@ -30,29 +30,6 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-# def past_days_news(request,past_date):
-#
-#     try:
-#         # Converts data from the string Url
-#         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-#
-#     except ValueError:
-#         # Raise 404 error when ValueError is thrown
-#         raise Http404()
-#
-#     # Converts data from the string Url
-#         day = convert_dates(date)
-#     html = f'''
-#         <html>
-#             <body>
-#                 <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-#             </body>
-#         </html>
-#             '''
-#     return HttpResponse(html)
-
-####
-
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
@@ -75,3 +52,25 @@ def past_days_news(request, past_date):
 
     news = Article.days_news(date)
     return render(request, 'all-news/past-news.html', {"date": date,"news":news})
+
+#search title on the navbar
+def search_results(request):
+
+    if 'article' in request.GET and request.GET["article"]:
+        search_term = request.GET.get("article")
+        searched_articles = Article.search_by_title(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-news/search.html',{"message":message,"articles": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-news/search.html',{"message":message})
+
+
+def article(request,article_id):
+    try:
+        article = Article.objects.get(id = article_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"all-news/article.html", {"article":article})
